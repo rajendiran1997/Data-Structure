1c860fb820dac5c6720e52e7932ef1c059ddb58a
rajendiran1997 <37899373+rajendiran1997@users.noreply.github.com>
2018-08-25 15:06:26 +0530

    Add files via upload

---
+++ EmailValidator.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;
+
+public class EmailValidator{
+    public static void main(String args[]){
+        Pattern p = Pattern.compile("([a-zA-Z%0-9_-]+[\\.0-9a-zA-Z]+@[a-zA-Z.]+[a-zA-Z]{2,3})");
+        Matcher m = p.matcher("mangoosem43%andayan@gmail.com");
+        
+        while(m.find()){
+            System.out.println(m.group());
+        }
+        //System.out.println();
+        if(m.find() == false){
+            System.out.println("Data not found");
+        }
+    }
+}
