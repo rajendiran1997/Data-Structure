3e238f7b8e063f5e714f4bf17af171b1640ea7ce
rajendiran1997 <37899373+rajendiran1997@users.noreply.github.com>
2018-09-24 21:04:33 +0530

    Update DOB.java

--- DOB.java
+++ DOB.java
@@ -3,10 +3,10 @@ public class DOB {
     public static void main(String args[]) {
         int cur_month = 9;
         int cur_year = 2018;
-     
-        int dob_month = 11;
+        
+        int dob_month = 12;
         int dob_year = 1997;
         
-        System.out.println((cur_month - dob_month)>=0?"Year "+ (cur_year - dob_year) + " Months "+ (cur_month - dob_month-1):"Year "+ ((cur_year - dob_year) - 1) + " Months "+ (11 -(dob_month - cur_month)));
+        System.out.println((cur_month - dob_month)>=0?"Years "+ (cur_year - dob_year) + " Months "+ (cur_month - dob_month):"Years "+ ((cur_year - dob_year) - 1) + " Months "+ (12 -(dob_month - cur_month)));
     }
 }
