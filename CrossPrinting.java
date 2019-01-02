d81ca01948fd808ef1243d3c8df29cc442357791
Rajendiran <37899373+rajendiran1997@users.noreply.github.com>
2019-01-02 18:36:48 +0530

    Create CrossPrinting.java

---
+++ CrossPrinting.java
@@ -0,0 +1,55 @@
+import static java.lang.System.out;
+
+public class CrossPrinting {
+    
+    static void crossPrint(char c[]) {
+        
+        boolean status = false;
+      
+        if(c.length/2 != 0) {
+            int length = c.length;
+            
+            for(int i = 0; i < c.length; i++) {
+                
+                 if(length < 0)
+                    status = true;
+                
+                if(!status)
+                    length -= 2;
+                else
+                    length += 2;
+                
+                for(int k = i; k<=(c.length/2) && k > 0 && status==false ; k--) {
+                        out.print(" ");
+                }
+                
+                for(int k = i; k<(c.length-1)&& status == true; k++) {
+                    out.print(" ");
+                }
+                
+                
+                out.print(c[i]);
+                
+                    
+                for(int j = 0; j < length; j++)
+                    out.print(" ");
+                
+                if(length > 0)
+                    out.print(c[i]);
+                
+                
+                out.println();
+            }
+        }
+        else
+        {
+            out.println("Give me odd number of letters");
+        }
+       
+    }
+    
+    public static void main(String args[]) {
+        String title = "rajendran";
+        crossPrint(title.toCharArray());
+    }
+}
